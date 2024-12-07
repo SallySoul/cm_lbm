2fc67fcb3a57d33e2b763e7758d572cf177aaeef
RussellBentley <russell.w.bentley@icloud.com>
2024-12-07 00:31:10 -0500

    Need to move on for now

--- cm_lbm/examples/eq_comparison.rs
+++ cm_lbm/examples/eq_comparison.rs
@@ -9,27 +9,40 @@ fn abs_density_f64(f: [f64; 27]) -> f64 {
 }
 
 fn main() {
-    let mut f = eq(0.0, 0.0, 0.1, 1.0);
+    let riv = 0.5;
+    let omega = 0.5;
+    let density = 1.0;
+    let mut f = eq(0.0, 0.0, 0.2, density);
     let mut m = moments(f);
     let mut da = abs_density(f);
 
-    let mut f_f64 = eq_f64(0.0, 0.0, 0.1, 1.0);
+    let mut f_f64 = eq_f64(0.0, 0.0, 0.2, density as f64);
     let mut m_f64 = moments_f64(f_f64);
     let mut da_f64 = abs_density_f64(f_f64);
 
-    println!("0, f32 - {} - {:?} -- {:?}", da, m, f);
-    println!("0, f64 + {} - {:?} -- {:?}", da_f64, m_f64, f_f64);
+    let mut f_bgk = eq(0.0, 0.0, 0.2, density);
+    let mut m_bgk = moments(f_bgk);
+    let mut da_bgk = abs_density(f_bgk);
+
+    println!("0, f32 - {} - {:?}", da, m);
+    println!("0, bgk - {} - {:?}", da_bgk, m_bgk);
+    println!("0, f64 + {} - {:?}\n", da_f64, m_f64);
 
     for i in 0..40 {
-        f = cm_mrt(f, m.1[0], m.1[1], m.1[2], m.0, 0.6);
+        f = cm_mrt(f, m.1[0], m.1[1], m.1[2], m.0, riv);
         m = moments(f);
         da = abs_density(f);
 
+        f_bgk = bgk(f_bgk, m_bgk.1[0], m_bgk.1[1], m_bgk.1[2], m_bgk.0, omega);
+        m_bgk = moments(f_bgk);
+        da_bgk = abs_density(f_bgk);
+
         f_f64 =
-            cm_mrt_f64(f_f64, m_f64.1[0], m_f64.1[1], m_f64.1[2], m_f64.0, 0.6);
+            cm_mrt_f64(f_f64, m_f64.1[0], m_f64.1[1], m_f64.1[2], m_f64.0, riv as f64);
         m_f64 = moments_f64(f_f64);
         da_f64 = abs_density_f64(f_f64);
-        println!("{}, f32 - {} - {:?} -- {:?}", i, da, m, f);
-        println!("{}, f64 + {} - {:?} -- {:?}", i, da_f64, m_f64, f_f64);
+        println!("{}, f32 - {} - {:?}", i, da, m);
+        println!("{}, bgk - {} - {:?}", i, da_bgk, m_bgk);
+        println!("{}, f64 + {} - {:?}\n", i, da_f64, m_f64);
     }
 }
